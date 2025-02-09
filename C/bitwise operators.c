4f0efc141dfb41a95f8bda3d2cdda675a9f80cc6
Adrian-isp <adrianrosu5400@gmail.com>
2025-02-09 23:56:00 +0200

    added and or xor

--- C/bitwise operators.c
+++ C/bitwise operators.c
@@ -3,12 +3,29 @@
 int main(){
     // Bitwise operators: used to manipulate binary bits in memory
     /*
-    & - AND
-    | - OR
-    ^ - XOR
-    >> - right shift
-    << - left shift
+        & - AND
+        | - OR
+        ^ - XOR
+        >> - right shift
+        << - left shift     
     */
+    int x = 6;  // 6 = 00000110 06
+    int y = 12; //12 = 00001100 0c
+    int z = 0;  // 0 = 00000000 00
+
+    printf("z0 = %d\n", z);
+
+    z = x & y;
+
+    printf("AND = %d\n", z);
+
+    z = x | y;
+
+    printf(" OR = %d\n", z);
+    
+    z = x ^ y;
+
+    printf("XOR = %d\n", z);
 
     return 0;
 }
