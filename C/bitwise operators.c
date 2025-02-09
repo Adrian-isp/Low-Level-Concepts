bb41a8975e2fda6001e7ec78bd2e8def17ae15fa
Adrian-isp <adrianrosu5400@gmail.com>
2025-02-09 23:50:01 +0200

    began the bitwise operators program

---
+++ C/bitwise operators.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    // Bitwise operators: used to manipulate binary bits in memory
+    /*
+    & - AND
+    | - OR
+    ^ - XOR
+    >> - right shift
+    << - left shift
+    */
+
+    return 0;
+}
