cf7d20f4a04bb3539407f3b9f6dcc602ba9cada9
Adrian-isp <adrianrosu5400@gmail.com>
2025-02-10 23:26:57 +0200

    added the memory learning file

---
+++ C/memory adresses.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    // memory = an array of bytes within RAM (street)
+    // memory block = a single unit (byte) within memory, used to store data (person)
+    // memory address = the address of where a memory is located in memory(home address)
+
+
+}
