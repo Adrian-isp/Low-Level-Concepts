b29a8c9b38651be747bdff76436092264d3d98bf
Adrian-isp <adrianrosu5400@gmail.com>
2025-02-15 18:27:33 +0200

    more assembly shoit I don't understand

---
+++ asm/arith.s
@@ -0,0 +1,47 @@
+	.file	"arithmetic.c"
+	.text
+	.globl	a
+	.data
+	.align 4
+a:
+	.long	10
+	.globl	b
+	.align 4
+b:
+	.long	5
+	.section .rdata,"dr"
+  
+.LC0:
+	.ascii "%d\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	movl	a(%rip), %edx
+	movl	b(%rip), %eax
+	addl	%edx, %eax
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %edx
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rcx
+	call	printf
+	movl	$0, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (Rev2, Built by MSYS2 project) 14.2.0"
+	.def	printf;	.scl	2;	.type	32;	.endef
---
+++ asm/arithmetic.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int a = 10;
+int b = 5;
+
+int main(){
+    int ADD = a+b;
+
+    printf("%d", ADD);
+
+    return 0;
+}
