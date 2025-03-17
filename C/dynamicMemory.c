9597a06418d6533dcf8bf6a5b19fa72580bb89d3
Adrian-isp <adrianrosu5400@gmail.com>
2025-03-17 22:44:04 +0200

    dynamic memory in C malloc calloc

--- C/dynamicMemory.c
+++ C/dynamicMemory.c
@@ -11,20 +11,31 @@
 
 int main() {
     // example allocating memory for 5 intagers:
-    int* arr = (int *)malloc(sizeof(int));
+    int* arr = (int *)malloc(sizeof(int) * 5);
+    
+    // example allocating 5 contiguous blocks:
+    int* carr = (int *)calloc(5, sizeof(int));
 
-    if(arr == NULL)
+    if(arr == NULL || carr == NULL)
         printf("Failed memory allocation\n");
     // returns NULL if allocation fails
     for(int i = 0; i < 5; i++) {
         arr[i] = i+1;
+        carr[i] = (i+1) * 10;
     }
     
-    printf("%d\n", arr[1]);
+    printf("%d\n", arr[0]);
+    printf("%p\n", arr);
+    
+    printf("%d\n", carr[0]);
+    printf("%p\n", carr);
 
     // always free pointers
     free(arr);
     arr = NULL;
+
+    free(carr);
+    carr = NULL;
 
     return 0;
 }
